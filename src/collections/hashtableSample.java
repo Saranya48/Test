c50bf1a6099b2270e9a1f7007a45ec0d92679363
saranya48 <saranya777mathiyalagan@gmail.com>
2019-02-20 18:27:00 +0530

    123

--- src/collections/hashtableSample.java
+++ src/collections/hashtableSample.java
@@ -8,10 +8,10 @@ public class hashtableSample {
 		Map<Integer, Collections3> ht = new Hashtable<>();
 		Collections3 c4 = new Collections3("saranya", 1, "sysveda");
 		Collections3 c2 = new Collections3("Abhishek", 2, "sysveda");
-		Collections3 c3 = new Collections3("selva", 3, "sysveda");
+		
 		ht.put(1, c4);
 		ht.put(3, c2);
-		ht.put(2, c3);
+		
 		for (Map.Entry<Integer, Collections3> m : ht.entrySet()) {
 			int k = m.getKey();
 			Collections3 i = m.getValue();
